162050fa82cc1cf555fca4e363d1d34c1c4cc384
Michael Mroz <mroz15@hotmail.com>
2023-01-06 16:58:12 -0400

    complete 10.13 - create useSignIn hook and authenticate user mutation to retrieve access token on SignIn form submit

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import theme from '../theme';
 import { Formik } from 'formik';
 import FormikTextInput from './FormikTextInput';
 import * as yup from 'yup';
+import useSignIn from '../hooks/useSignIn';
 
 const styles = StyleSheet.create({
     container: {
@@ -47,8 +48,17 @@ const initialValues = {
 
 const SignIn = () => {
 
-  const onSubmit = (values) => {
-    console.log(values) //temporary for exercise 10.8
+  const [signIn] = useSignIn();
+  
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+    
+    try{
+      const { data } = await signIn({ username, password } );
+      console.log(data)
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   const validationSchema = yup.object().shape({
---
+++ src/graphql/mutations.js
@@ -0,0 +1,8 @@
+import { gql } from "@apollo/client";
+
+export const AUTHENTICATE_USER = gql`
+    mutation AuthenticateUser($credentials: AuthenticateInput) {
+        authenticate(credentials: $credentials) {
+            accessToken
+        }
+    }`
