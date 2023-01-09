9383849dd31df70e9777605ae4a56686c5460f79
Michael Mroz <mroz15@hotmail.com>
2023-01-09 15:09:43 -0400

    add useNavigate hook to SignIn to redirect user to main page on successful sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { View, StyleSheet, Pressable } from 'react-native';
+import { useNavigate } from 'react-router-native';
 import Text from './Text';
 import theme from '../theme';
 import { Formik } from 'formik';
@@ -49,13 +50,15 @@ const initialValues = {
 const SignIn = () => {
 
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
   
   const onSubmit = async (values) => {
     const { username, password } = values;
     
-    try{
-      const { data } = await signIn({ username, password } );
-      console.log(data)
+    try {
+      await signIn({ username, password } );
+      navigate('/')
+      
     } catch (e) {
       console.log(e)
     }
