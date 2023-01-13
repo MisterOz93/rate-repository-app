7a532daa4a6a9d514af8c90d99c8ce758549f529
Michael Mroz <mroz15@hotmail.com>
2023-01-12 20:47:13 -0400

    create view structure of review component. Still needs to be styled properly

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,13 +1,56 @@
-import { FlatList } from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
 import { useParams } from "react-router-native";
 import { GET_REPOSITORY } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
 import RepositoryItem from "./RepositoryItem";
+import theme from "../theme";
 
+const styles = StyleSheet.create({
+    reviewContainer: {
+        backgroundColor: theme.colors.itemBackground,
+    },
+
+    reviewHeader: {
+        flexDirection: 'row ',
+    },
+
+    reviewRating: {
+        borderStyle: 'solid',
+        borderColor: theme.colors.borderColor,
+    },
+
+    reviewAuthorAndDate: {
+
+    },
+
+    separator: {
+        height: 10,
+      },
+})
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const ReviewItem = ({review}) =>  {
-    console.log('review passed to reviewItem is', review)
-    return <h2>Hi</h2>
+    //console.log('review passed to reviewItem is', review)
+    const reviewDateArr = review.createdAt.split('-');
+    const months = {'01': 'Jan', '02': 'Feb', '03': 'March', '04': 'Apr', '05': 'May', '06': 'June', '07': 'July',
+                     '08': 'Aug', '09': 'Sept', '10': 'Oct', '11': 'Nov', '12': 'Dec'}
+    const reviewDate = `${months[reviewDateArr[1]]}.${reviewDateArr[2].substring(0, 2)}.${reviewDateArr[0]}`;
+
+    return(
+        <View style={styles.reviewContainer}>
+            <View style={styles.reviewHeader}>
+                <View style={styles.reviewRating}>
+                    <Text>{review.rating}</Text>
+                </View>
+                <View style={styles.reviewAuthorAndDate}>
+                    <Text>{review.user.username}</Text>
+                    <Text>{reviewDate}</Text>
+                </View>
+            </View>
+            <Text>{review.text}</Text>
+        </View>
+    )
 }
 
 const SingleRepository = () => {
@@ -31,6 +74,7 @@ const SingleRepository = () => {
             renderItem={({ item }) => <ReviewItem review={item} />}
             keyExtractor={({ id }) => id}
             ListHeaderComponent={() => <RepositoryItem item={repository} singleView/>}
+            ItemSeparatorComponent={ItemSeparator}
         />
     )
 }
